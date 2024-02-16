0bfd3205f76532e05e703c5351d6fd5f3d1d2ee0
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-02-16 15:15:04 -0500

    library assignment good copy

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+let newBookButton = document.querySelector("#add-Book-Button");
+newBookButton.addEventListener("click", function(){
+    let newBookForm = document.querySelector(".form-slip-container");
+    newBookForm.style.display = "block"
+})
